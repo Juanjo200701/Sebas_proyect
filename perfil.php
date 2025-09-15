646b3c375e6460cc32f6eeff220a11bfad464637
Rodrigo Serrano <rodrigoserranov40@gmail.com>
2025-09-14 19:58:24 -0500

    parte del perfil con estilos

---
+++ perfil.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style/todo.css">
+</head>
+<body>
+<div class="profile-container">
+  <div class="profile-header">
+    <img src="ruta-de-tu-imagen.jpg" alt="Foto de perfil" class="profile-avatar">
+    <h2>Nombre del Usuario</h2>
+    <p class="profile-email">usuario@email.com</p>
+  </div>
+
+  <div class="profile-actions">
+    <button onclick="cerrarSesion()">Cerrar sesión</button>
+    <button onclick="editarPerfil()">Editar perfil</button>
+  </div>
+</div>
+
+</body>
+</html>
